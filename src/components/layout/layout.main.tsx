d073b7f57ee528cd892ea5700242c95e40b472d0
Serhii Hrudakov <avatarcapa@gmail.com>
2026-02-19 22:57:16 +0100

    feat: Render VacancyView on main

--- src/components/layout/layout.main.tsx
+++ src/components/layout/layout.main.tsx
@@ -1,7 +1,11 @@
+import { VacancyView } from './VacancyCard/VacancyView.tsx'
+
 export function MainLayout() {
   return (
     <div className="flex h-screen overflow-hidden bg-background">
-      <div className="flex flex-col flex-1 min-w-0 overflow-hidden"></div>
+      <div className="flex flex-col flex-1 min-w-0 overflow-hidden">
+        <VacancyView />
+      </div>
     </div>
   )
 }
