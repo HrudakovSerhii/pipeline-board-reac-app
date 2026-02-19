2d99b59b0763c6629bcdfc8e1c848dbe87110fb0
Serhii Hrudakov <avatarcapa@gmail.com>
2026-02-19 22:29:16 +0100

    feat: Pill component (style wrapper)

--- src/components/ui/Pill/Pill.tsx
+++ src/components/ui/Pill/Pill.tsx
@@ -10,7 +10,7 @@ export function Pill({ children, className }: PillProps) {
   return (
     <div
       className={cn(
-        'inline-flex items-center h-[22px] px-[8px] rounded-full bg-pill-bg text-[11px] text-secondary whitespace-nowrap',
+        'inline-flex items-center h-5.5 px-2 rounded-full text-xs border-input-border bg-white text-secondary whitespace-nowrap',
         className,
       )}
     >
