7b8a742ce9cd864b2d075b6cb0e7b629a0674240
Serhii Hrudakov <avatarcapa@gmail.com>
2026-02-19 21:50:04 +0100

    setup: connected tailwind to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 import { mockApi } from './middleware/api.middleware.ts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mockApi()],
+  plugins: [tailwindcss(), react(), mockApi()],
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
