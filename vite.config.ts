ee16f1b2e6ba07c6908636d1a346620492f43db1
Serhii Hrudakov <avatarcapa@gmail.com>
2026-02-20 02:21:10 +0100

    chore: prod. build vite config refined - code cleanup - test coverage - meta tags for index doc

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,34 @@ import tailwindcss from '@tailwindcss/vite'
 import { mockApi } from './middleware/api.middleware.ts'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [tailwindcss(), react(), mockApi()],
+  build: {
+    target: 'es2020',
+    reportCompressedSize: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'vendor'
+          }
+          if (id.includes('node_modules/@dnd-kit')) {
+            return 'dnd'
+          }
+        },
+      },
+    },
+  },
+  esbuild: {
+    drop: mode === 'production' ? ['console', 'debugger'] : [],
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+    },
   },
-})
+}))
